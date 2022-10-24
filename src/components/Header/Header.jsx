8b1d52581f39518bc4f0dae10dca7faa263b1c9d
PTMAndrey <dorinandries766@yahoo.com>
2022-10-24 11:01:44 +0300

    header

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import ReactLogo from "../../assets/images/Header.svg";
 import styles from "./Header.module.scss";
-import {BsFillArrowDownCircleFill} from "react-icons/bs"
+import { BsFillArrowDownCircleFill } from "react-icons/bs"
 
 
 const Header = () => {
@@ -9,18 +9,16 @@ const Header = () => {
     <>
       <header style={{ backgroundImage: `url('${ReactLogo}')`, backgroundRepeat: 'no-repeat', backgroundSize: 'cover', height: '540px', width: '100%' }}>
         <div className={styles.siteBanner}>
-          <div className={styles.overlay}>
-            <div className='d-flex justify-content-center align-items-center h-100 pt-5'>
-              <div className='text-white p-5'>
-                <h1 className='mb-3'><strong>C.S.M. SUCEAVA</strong></h1>
-                <h4 className='mb-3'><b>ECHIPA DE VOLEI</b></h4>
-                {/* <div className={styles.arrowRotate}>
+          <div className='d-flex justify-content-center align-items-center h-100 pt-5'>
+            <div className='text-white p-5'>
+              <h1 className='mb-3 pt-5'><strong>C.S.M. SUCEAVA</strong></h1>
+              <h4 className='mb-3'><b>ECHIPA DE VOLEI</b></h4>
+              {/* <div className={styles.arrowRotate}>
                   <span><BsFillArrowDownCircleFill/></span>
                 </div> */}
-                {/* <a className='btn btn-outline-light btn-lg' href='#!' role='button'>
+              {/* <a className='btn btn-outline-light btn-lg' href='#!' role='button'>
                     Call to action
                   </a> */}
-              </div>
             </div>
           </div>
         </div>
