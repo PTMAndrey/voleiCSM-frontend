eca9f9c676c21aa49a00f175688fa116387c5fe2
PTMAndrey <dorinandries766@yahoo.com>
2022-10-25 23:51:26 +0300

    header styling

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -9,7 +9,7 @@ const Header = () => {
       <header style={{ backgroundImage: `url('${ReactLogo}')`, backgroundRepeat: 'no-repeat', backgroundSize: 'cover', height: '540px', width: '100%' }}>
         <div className={styles.siteBanner}>
           <div className='d-flex justify-content-center align-items-center h-100 pt-5'>
-            <div className='text-white p-5 `${styles.text-header}`'>
+            <div className={`text-white p-5 ${styles.textHeader}`}>
               <h1 className='mb-3 pt-5'><strong>C.S.M. SUCEAVA</strong></h1>
               <h4 className='mb-3'><b>ECHIPA DE VOLEI</b></h4>
               {/* <div className={styles.arrowRotate}>
